e325a985a6ec909e68e1907efe54c790bde85390
IgorMeloMoraes <igor-batista@outlook.com.br>
2024-03-05 16:02:36 -0300

    Aulas - Intro, Primeiro Programa, Atalhos, System.out.println, comentarios e organização

---
+++ Fundamentos/src/fundamentos/PrimeiroPrograma.java
@@ -0,0 +1,51 @@
+package fundamentos;
+
+public class PrimeiroPrograma {
+	
+	/*
+	  -> WORKSPACE 
+		  . Todas as preferencias que voce decidir, fica armazenado dentro do Workspace.
+		  . Area de Trabalho
+		  . Para trocar workspace basta ir em File - Switch Workspace
+		  . Ele cria uma pasta oculta .metadata carregando nossas preferencias criadas
+		  . É basicamente uma pasta onde armazenamos nossos codigos
+
+	 -> PERSPECTIVA 
+		  . Em window + perspective
+		    . Podemos definir as Perspectiva de telas que queremos
+		  . Vamos trabalhar na pespectiva Java 
+		  
+	-> PACKAGE 
+		. Funciona basicamente como os nossos diretorios de arquivos 
+		   
+	 
+	  -> ATALHOS 
+	   		. CTLR + ESPAÇO = Abre um leque de possibilidades de completar nosso codigo 
+			   	. Main - Ele cria a classe principal do nosso programa 
+			   	. Syso - Ele cria uma saida do sistema 
+	   		. CTLR + SFIT + F11 - Roda nosso codigo no console 
+	   	
+	   	
+	   -> Dicas IDE 
+	   		. Podemos arrastar e colocar as janelas da forma que queremos e onde queremos 
+	   	
+	  
+	  -> COMO UM CODIGO JAVA É ORGANIZADO ? 
+	   		. Como organizamos um livro: Podemos sair do macro para o micro, pensando que um livro pode ser organizado por capitulos, que é organizado por paragrafos, que são organizados em palavras, em letras... 
+	 		. Ele é organizado em sentenças de codigo que terminam com ponto e virgula (;)
+	 		. O que vai indicar uma sequencia de codigo não é a quantidade de linhas e sim um ponto e virgula 
+	 		. O uso de ponto e virgula é obrigatorio
+	 		. Podemos quebrar o codigo em qualquer ponto? nao, podemos quebrar em alguns pontos especificos
+	 		. Utilizamos frequentemente pares de chaves
+	 		. As chaves significam blocos de codigo 
+	 		. Serve para agrupar sentenças de codigo
+	 		. Representa um paragrafo  */ 
+	   	
+	
+	 public static void main(String[] args) {
+		 
+		 // Imprima uma linha na saida do sistema
+		System.out.println("Hello World!");
+	}
+
+}
